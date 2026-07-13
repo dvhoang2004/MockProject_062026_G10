edc24c1d0b1079da63045d0a4ddbc1fe0a13c7a4
dvhoang2004 <dvh122004@gmail.com>
2026-07-13 16:52:04 +0700

    feat(sla): add sla page UI

--- src/routes/Routing/Routing.jsx
+++ src/routes/Routing/Routing.jsx
@@ -17,6 +17,7 @@ import Facility from "../../pages/Facility/Facility";
 import LOCPage from "../../pages/LOC/LOC";
 import Staffing from "../../pages/Staffing/Staffing";
 import IncidentSeverityLevel from "../../pages/IncidentSeverityLevel/IncidentSeverityLevel";
+import SLA from "../../pages/SLA/SLA";
 import Login from "../../pages/Auth/Login/Login";
 import AccountActivation from "../../pages/Auth/AccountActivation/AccountActivation";
 import Verification from "../../pages/Auth/Verification/Verification";
@@ -38,6 +39,7 @@ const Routing = () => {
       <Route path="/level-of-care" element={<LOCPage />} />
       <Route path="/staffing" element={<Staffing />} />
       <Route path="/incident-severity" element={<IncidentSeverityLevel />} />
+      <Route path="/sla" element={<SLA />} />
       <Route path="/" element={<Navigate to="/resident-list" replace />} />
       <Route path="/resident-list" element={<ResidentList />} />
       <Route path="/dashboard-nurse" element={<DashboardNurse />} />
